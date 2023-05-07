d56f0608d1d3a9f1d05e671053065c8e782f40eb
NoahJenkins <noah@noahjenkins.com>
2023-05-07 09:44:31 -0500

    Tip_Calculator

--- Day2/Time_Until_90_Calculator.py
+++ Day2/Time_Until_90_Calculator.py
@@ -7,3 +7,5 @@ WU90 = YU90 * 52
 DU90 = YU90 * 365
 
 print(f"You have {YU90} years, {WU90} weeks, & {DU90} days until you are 90 years of age.")
+
+#This is a program that calculates how many days, weeks, and years you have left until you are 90 years of age.
