38d7e993b1f6f3ddadc7a1913ec6af0181ad4305
NoahJenkins <noah@noahjenkins.com>
2023-05-07 08:17:58 -0500

    Day 2 notes Data types

---
+++ Day2/Day_2_SB.py
@@ -0,0 +1,31 @@
+ # Data Types
+ 
+# String
+
+print("Hello"[4])
+#This is a subscripting, where we can access a specific character in a string.
+
+"123"
+#This is a string, which is a sequence of characters. Any characerters in the quotes will be a string.
+
+#Integer
+
+123
+#This is an integer, which is a whole number.
+
+print(123 + 345)
+#You can do math with integers.
+
+123_456_789
+#You can use underscores to make large numbers easier to read.
+
+#Float
+
+3.14159
+#This is a float, which is a decimal number.
+
+#Boolean
+
+True
+False
+#These are booleans, which are true or false values.
