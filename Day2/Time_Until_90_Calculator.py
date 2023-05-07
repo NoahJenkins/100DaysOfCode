364731cc1072e6fcba06a112b0a8e66564d2b47d
NoahJenkins <noah@noahjenkins.com>
2023-05-07 09:27:16 -0500

    test commit

---
+++ Day2/Time_Until_90_Calculator.py
@@ -0,0 +1,9 @@
+age = input("What is your current age? ")
+
+YU90 = 90 - int(age) 
+
+WU90 = YU90 * 52
+
+DU90 = YU90 * 365
+
+print(f"You have {YU90} years, {WU90} weeks, & {DU90} days until you are 90 years of age.")
---
+++ Day2/Type_Conversions.py
@@ -0,0 +1,17 @@
+num_char = len(input("What is your name?"))
+#print("Your name has " + num_char + " characters.")
+
+# Type Conversion
+
+str(num_char)
+#converts the number to string
+
+new_num_char = str(num_char)
+
+print("Your name has " + new_num_char + " characters.")
+
+
+#You can also convert to int inside the print statement;
+
+print("Your nmae has " + str(num_char) + " characters.")
+print(type(num_char))
