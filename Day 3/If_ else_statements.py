73ba77639dd547218564a91c9e6a6320fb4ebbf4
NoahJenkins <noah@noahjenkins.com>
2023-05-08 15:25:29 -0500

    mid life crisis addition to ticket system

--- Day 3/If_ else_statements.py
+++ Day 3/If_ else_statements.py
@@ -1,36 +1,3 @@
-
-
-  #what are comparison operators?
-    # > greater than
-    # < less than
-    # >= greater than or equal to
-    # <= less than or equal to
-    # == equal to
-    # != not equal to
-
-
-#Nesting if statements and elif statements in python
-# elif is short for else if
-# elif is used when you have more than 2 conditions
-# if you have more than 2 conditions, you can use elif
-# example:
-# if condition1:
-#   do A
-# elif condition2:
-#   do B
-# elif condition3:
-#   do C
-# else:
-#   do D
-# if condition1 is true, then do A
-# if condition1 is false, then check condition2
-# if condition2 is true, then do B
-# if condition2 is false, then check condition3
-# if condition3 is true, then do C
-# if condition3 is false, then do D
-# if none of the conditions are true, then do D
-
-
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 bill = 0
@@ -44,6 +11,9 @@ if height >= 120:
   elif age <= 18:
     bill = 7
     print("Youth tickets are $7.")
+  elif age >= 45 and age <= 55:
+    bill = 0
+    print("Everything is going to be ok. Have a free ride on us!")
   else:
     bill = 12
     print("Adult tickets are $12.")
---
+++ Day 3/logical_operators.py
@@ -0,0 +1,11 @@
+#logical operators
+#and, or, not
+
+#and
+print(True and True) #all values must be true to return true
+
+#or
+print(True or False) #only one value must be true to return true
+
+#not
+print(not True) #returns the opposite of the value
