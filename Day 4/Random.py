2942ee329a90eba33e3c47c4c34615362503527a
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-05-11 07:51:48 -0500

    random

---
+++ Day 4/Random.py
@@ -0,0 +1,13 @@
+import random
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+#what is a module?
+# a module is a piece of code that someone else has written that you can use in your program
+# you can import modules from the python standard library or from third party libraries using pip
+# pip is a package manager that allows you to install third party libraries
+
+# you can also import modules from your own code
+# you can create a module by creating a new python file and importing it into your main file
+# you can also create a module by creating a new folder and adding an __init__.py file
---
+++ Day 4/heads_tails.py
@@ -0,0 +1,8 @@
+import random
+
+result = random.randint(0,1)
+
+if result == 1:
+    print("Heads")
+else:
+    print("Tails")
