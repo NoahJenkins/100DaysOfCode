b22683275be5880ae88b7812669e1d0eefa523af
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-05-11 19:13:42 -0500

    random

---
+++ Day 4/list.py
@@ -0,0 +1,9 @@
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
+
+# or
