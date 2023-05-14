2941a00d63fc2aa5e49202e2a5aa7d6d9a20fdcd
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-05-14 13:01:52 -0500

    Fizz Buzz elif

--- Day 5/Fizz_Buzz.py
+++ Day 5/Fizz_Buzz.py
@@ -1,9 +1,9 @@
 for number in range(1,101,):
     if number % 5 == 0 and number % 3 == 0:
       print("Fizz Buzz")
-    if number % 5 == 0:
+    elif number % 5 == 0:
       print("Buzz")
-    if number % 3 == 0:
+    elif number % 3 == 0:
       print("Fizz ")
     else:
       print(number)
