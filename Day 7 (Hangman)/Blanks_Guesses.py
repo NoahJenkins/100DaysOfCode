a0f6678a4980dadd06632065de83a299aae9830f
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-05-15 18:26:56 -0500

    product

---
+++ Day 7 (Hangman)/Blanks_Guesses.py
@@ -0,0 +1,30 @@
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+display = ["_"] * len(chosen_word)  # Create a list of underscores with the same length as chosen_word
+print(display)
+# Testing code
+print(f"Pssst, the solution is {chosen_word}.")
+
+guess = input("Guess a letter: ").lower()
+
+# Loop through each position in the chosen_word
+for position in range(len(chosen_word)):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
+
+print(display)
+
+
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
+
+
+
+print(display)
