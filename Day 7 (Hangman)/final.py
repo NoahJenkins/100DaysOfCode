0c1a361e809a84c6a3af7343168f36493124300e
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-05-18 17:24:17 -0500

    test

--- Day 7 (Hangman)/final.py
+++ Day 7 (Hangman)/final.py
@@ -47,6 +47,7 @@ while not end_of_game:
         if lives == 0:
             end_of_game = True
             print("You lose.")
+            print(f"The word was {chosen_word}")
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
