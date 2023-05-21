9969a94e711a4fb325a248d746f758f2111b94e6
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-05-21 10:32:26 -0500

    decryption

---
+++ Day 8 (Parameters and Inputs)/Ceaser Cypher (Project)/Decryption.py
@@ -0,0 +1,28 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(plain_text, shift_amount):
+  cipher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    cipher_text += alphabet[new_position]
+  print(f"The encoded text is {cipher_text}")
+
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+
+def decrypt(cipher_text, shift_amount):
+  decrypted_text = ""
+  for letter in cipher_text:
+    position = alphabet.index(letter)
+    new_positom = position - shift_amount
+    decrypted_text += alphabet[new_positom]
+  print(f'The decrypted message is: "{decrypted_text}"')
+    
+if direction == 'encode':
+  encrypt(plain_text=text, shift_amount=shift)
+elif direction == 'decode':
+  decrypt(cipher_text=text, shift_amount =shift)
