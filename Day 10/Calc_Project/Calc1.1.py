5a9380a921dbedc820fa6b333bca0ad7561ac858
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-05-27 17:14:20 -0500

    calc 1.1

---
+++ Day 10/Calc_Project/Calc1.1.py
@@ -0,0 +1,57 @@
+from art import logo 
+
+print(logo)
+
+#calculator
+
+#Add 
+def add(n1, n2):
+  return n1 + n2
+
+#Subtract
+def subtract(n1,n2):
+  return n1 - n2
+
+#Multiply
+def multiply(n1,n2):
+  return n1 * n2
+
+#Divide
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide
+}
+
+def calculator():
+  for key in operations.keys():
+      print(key)
+  
+  num1 = int(input("What's the first number?: "))
+  
+  thinking = True
+  
+  while thinking == True:
+    num2 = int(input("What's the second number?: "))
+    operation_symbol = input("Pick an operation: ")
+  
+    if operation_symbol in operations:
+        operation_func = operations[operation_symbol]
+        answer = operation_func(num1, num2)
+      
+    print(f'{num1} {operation_symbol} {num2} = {answer}')
+      
+    choice = input("Type 'y' to continue calculating with {answer}, or type 'n' to start a fresh calculation: ").lower()
+  
+    if choice == 'y':
+        num1 = answer
+    else:
+        thinking = False
+        calculator()
+
+
+calculator()
