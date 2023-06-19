7b0c1e4f9ba51c8ebf2e6158bcd2b2212b57b25d
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-06-19 14:55:36 -0500

    day 14 1 & 1.1

--- Day 14 (Higher or Lower Game)/Higher_Lower_1.1.py
+++ Day 14 (Higher or Lower Game)/Higher_Lower_1.1.py
@@ -1,21 +1,17 @@
-#Higher or Lower Game
-# To Do List:
-#   1. Import Random
-#   2. Import game_data
-#   3. Import art
-#   4. Set up While Loop (game running)
-#   5. Create Logic to ask for 2 sets of values from data. 
-#   6. Create logic to compare both sets of data to see which is higher. 
-#   7. Create logic to ask for user input selecting which is higher. 
-#   8. Create logic to dermine if user wins or loses. 
-#   9. create logic to keep track of score. 
-#   10. Ending game logic (print score)
-
 
 import random
 from art import logo
 from art import vs
 from game_data import data
+import os
+
+def clear():
+    # Clear the terminal screen for Windows
+    if os.name == 'nt':
+        os.system('cls')
+    # Clear the terminal screen for Unix/Linux/MacOS
+    else:
+        os.system('clear')
 
 def run_game():
     score = 0
@@ -35,23 +31,21 @@ def run_game():
         
         if choice_A > choice_B and user_choice == 'a':
             score += 1
-            print("That is correct!")
+            print(f'That is correct! Your score is {score}')
         elif choice_A < choice_B and user_choice == 'b':
             score += 1
-            print("That is correct!")
+            print(f'That is correct! Your score is {score}')
         else:
-            print("That is incorrect!")
+            print(f'That is incorrect! Your final score is {score}')
             game_running = False
 
-        print(f"Your score is {score}")
-        print()
 
-        continue_game = input("Do you want to continue? Type 'y' or 'n': ").lower()
-        if continue_game != 'y':
-            game_running = False
-
-    print("Game Over")
-    print(f"Your final score is {score}")
+    continue_game = input("Do you want to continue? Type 'y' or 'n': ").lower()
+    if continue_game != 'y':
+        game_running = True
+    else:
+        print("Game Over")
+        print(f"Your final score is {score}")
 
 run_game()
 
