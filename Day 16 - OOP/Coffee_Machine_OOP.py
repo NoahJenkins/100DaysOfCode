e93ec8d9b6f73e556bb50681a1134190ea49e65c
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-06-25 13:33:52 -0500

    pypi, change version.

---
+++ Day 16 - OOP/Coffee_Machine_OOP.py
@@ -0,0 +1,8 @@
+#Steve Jobs OOP Quote: "Object-oriented programming teaches you to think about the world in terms of objects that interact with one another. "
+
+#Coffee Machine OOP
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
--- Day 16 - OOP/Packages_PyPi.py
+++ Day 16 - OOP/Packages_PyPi.py
@@ -1,9 +1,14 @@
 
 import prettytable
 
+# Creates a table object, this is a class of the prettytable package.
 table = prettytable.PrettyTable()
+
+# Adds columns to the table object, this is a method of the table object.
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"]) 
 table.add_column("Type", ["Electric", "Water", "Fire"])
+
+# Aligns the text to the left of the column, this is an attribute of the table object.
 table.align = "l"
 print(table)
 
