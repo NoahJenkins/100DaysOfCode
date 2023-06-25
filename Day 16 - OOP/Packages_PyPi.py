721b44531f97652ad9840fc22ba486b0768503f6
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-06-25 12:15:57 -0500

    pypi, change version.

--- Day 16 - OOP/Packages_PyPi.py
+++ Day 16 - OOP/Packages_PyPi.py
@@ -1,7 +1,20 @@
-from prettytable import PrettyTable
 
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+import prettytable
+
+table = prettytable.PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"]) 
 table.add_column("Type", ["Electric", "Water", "Fire"])
 table.align = "l"
 print(table)
+
+# Output:
+# +---------------+----------+
+# | Pokemon Name  | Type     |
+# +---------------+----------+
+# | Pikachu       | Electric |
+# | Squirtle      | Water    |
+# | Charmander    | Fire     |
+# +---------------+----------+
+
+#Note that prettytable is not currently supported in Python 3.11.4, I used Python 3.9.13 to run this code.
+
