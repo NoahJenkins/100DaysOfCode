97bf7500eccb48ec30ca269399c2574d31d64774
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-09 07:15:34 -0500

    day 8 set up

---
+++ Day 8 (Parameters and Inputs)/2nd_Go/functions_with_inputs.py
@@ -0,0 +1,17 @@
+#def my_function(something):
+    #Do this with something
+    #Then do this
+    #Finally do this
+
+# def greet():
+#     print('Hello')
+#     print('My name is Noah')
+#     print('What is your name?')
+
+
+
+def greet(name):
+    print(f'Hello {name}')
+
+greet('Noah')
+
