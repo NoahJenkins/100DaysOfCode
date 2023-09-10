0b616bc5c07f0d55ac105ff48ec7d3d56d1ccd1d
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-10 09:10:13 -0500

    auction start

---
+++ Day 9 (Dictionaries, Nesting)/2nd_Go/Silent_Auction.py
@@ -0,0 +1,9 @@
+import os
+
+def clear():
+    # Clear the terminal screen for Windows
+    if os.name == 'nt':
+        os.system('cls')
+    # Clear the terminal screen for Unix/Linux/MacOS
+    else:
+        os.system('clear')
