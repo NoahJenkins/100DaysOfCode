2d233e60324535d0144ab7ba4258c62806446ce6
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-10 08:41:15 -0500

    nesting

---
+++ Day 9 (Dictionaries, Nesting)/2nd_Go/nesting.py
@@ -0,0 +1,33 @@
+#Nesting
+captials = {
+"Feance": "Paris",
+"Germany": "Berlin"
+}
+
+#Nesting List in Dictionaries
+
+travel_log = {
+"Feance": ["Paris", "Lillie", "Dijon"],
+"Germany": ["berlin", "Hamburg", "Stutgart"]
+}
+
+
+#Nesting Dictionary in Dictionaries
+
+travel_log = {
+"Feance": {"cities_visited": ["Paris", "Lillie", "Dijon"]},
+"Germany": ["berlin", "Hamburg", "Stutgart"]
+}
+
+#Nesting a Dictionary in a List
+
+travel_log = [
+{"Key1": "Value1",
+ "Key2": "Value2",
+ "Key3": "Value3"
+ },
+ {"KeyA": "ValueA",
+ "KeyB": "ValueB",
+ "KeyC": "ValueC"
+ }
+]
