ee08c6dbc7b56f80e0220f47ca59c587869b1fa8
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-10 19:53:52 -0500

    Day 10 Start

--- Day 10 (Functions with Outputs)/Feb_Leap_Year.py
+++ Day 10 (Functions with Outputs)/Feb_Leap_Year.py
@@ -11,13 +11,14 @@ def is_leap(year):
     return False
 
 def days_in_month(year, month):
-  if month > 12 or month < 1:
-    return "Invalid month."
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  if is_leap(year) and month == 2:
-    return 29
-  return month_days[month - 1]
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) and month ==2:
+      return 29
+    return month_days[month -1]
 
+
+
+#🚨 Do NOT change any of the code below 
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
--- Day 10 (Functions with Outputs)/Functions_With_Outputs.py
+++ Day 10 (Functions with Outputs)/Functions_With_Outputs.py
@@ -1,22 +1,29 @@
 #Functions with outputs:
 
-def format_name(f_name, l_name):
-  print(f_name.title())
-  print(l_name.title())
+# def format_name(f_name, l_name):
+#   formatted_f_name = (f_name.title())
+#   formatted_l_name = (l_name.title())
 
-format_name("noah", "JENKINS")
+#   print(f'{formatted_f_name} {formatted_l_name}')
 
-def format_name(f_name, l_name):
-  first = f_name.title()
-  last = l_name.title()
-  print(f'{first} {last}')
-
-format_name("noah", "JENKINS")
+# format_name('NOAH', "jenkIns")
 
 def format_name(f_name, l_name):
-  first = f_name.title()
-  last = l_name.title()
-  return(f'{first} {last}')
+  formatted_f_name = (f_name.title())
+  formatted_l_name = (l_name.title())
 
-formated_string=format_name("noah", "JENKINS")
+  return(f'{formatted_f_name} {formatted_l_name}')
+#The return marks the end of the function, so any code after a return statement won't be executed.
+
+formated_string = format_name('NOAH', "jenkIns")
 print(formated_string)
+
+#Return explained:
+
+# when you want candy from the vending machine, what do you need? the inputs to the vending machine "function" are money and the code for the item you want. the output of the vending machine "function" is the candy, which you then eat.
+# so the vending machine "function" RETURNS candy. to you, the customer, the vending machine "function" is a black box: you don't care how the machine retrieves the item, all you care about is getting the correct item.
+# now imagine that it's your job to build the vending machine. it's not a black box anymore. as the designer of the machine, you must build it so that when the money and the code are entered, the mechanism inside gets and spits out the candy. you are in charge of making sure the machine RETURNS the item correctly.
+# when you write a function, you are in charge of making sure the function returns what the caller is expecting. the caller could be another function! in the vending machine case, the caller was the customer. if the customer wants a Snickers, then a Bag of Pretzels won't work. similarly, if the caller is another function, and it's expecting an Integer, then a String won't work.
+# we typically use return to link functions together based on the result of a computation. the data type of that result is often important.
+# not everything needs to be printed to the screen. often your program will have several functions exchanging values with each other before anything is printed out.
+
