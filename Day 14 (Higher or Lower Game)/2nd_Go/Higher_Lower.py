1204c5639585d6b39d3db2a3f3c421e113161e4d
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-15 17:01:55 -0500

    higher_lower complete

--- Day 14 (Higher or Lower Game)/2nd_Go/Higher_Lower.py
+++ Day 14 (Higher or Lower Game)/2nd_Go/Higher_Lower.py
@@ -1,53 +1,72 @@
 import random
+import os
 from art import logo
 from art import vs
 from game_data import data
 
-print(logo)
+
+def clear():
+    """This function will cleae the terminal. Please note that you must 'import os' for this to work."""
+    # Clear the terminal screen for Windows
+    if os.name == 'nt':
+        os.system('cls')
+    # Clear the terminal screen for Unix/Linux/MacOS
+    else:
+        os.system('clear')
 
 def grab_data():
     random_subject = random.choice(data)
     name = random_subject['name']
     follower = random_subject['follower_count']
-    descriptuon = random_subject['description']
+    description = random_subject['description']
     country = random_subject['country']
-    return name, follower, descriptuon, country
+    return name, follower, description, country
 
 subject_A = grab_data()
 subject_B = grab_data()
-subject_A_Value = subject_A[1]
-subject_B_Value = subject_B[1]
-
-if subject_A[0] == subject_B [0]:
-    subject_B = grab_data()
-# print(f'{subject_B[0]}')
-
-subject_A_Value = subject_A[1]
-subject_B_Value = subject_B[1]
-
-print(f'Compare A: {subject_A[0]}, a {subject_A[2]}, from {subject_A[3]}')
-print(subject_A_Value)
-
-print(vs)
-
-print(f'Compare B: {subject_B[0]}, a {subject_B[2]}, from {subject_B[3]}')
-print(subject_B_Value)
-
-def compare(subject_A, subject_B):
-    
-    if subject_A > subject_B:
-        answer = subject_A
-    else:
-        answer = subject_B
-    question = input('Who do you think has a higher follower count? A or B: ').lower
-    if answer == question:
-        return 1
-    else:
-        return 0
 
 score = 0
 
-score += int(compare(subject_A, subject_B))
-print(score)
+
+game_running = True
+
+while game_running:
+    print(logo)
+
+    print(f'The current score is {score}')
+    print(f'Compare A: {subject_A[0]}, a {subject_A[2]}, from {subject_A[3]}')
+    print(subject_A[1])
+
+    print(vs)
+
+    print(f'Compare B: {subject_B[0]}, a {subject_B[2]}, from {subject_B[3]}')
+    print(subject_B[1])
+
+    if subject_A[1] > subject_B[1]:
+        answer = 'a'
+    else:
+        answer = 'b'
+
+    question = input('Who do you think has a higher follower count? A or B: ').lower()
+
+    
+    if answer == question:
+        score += 1
+        if subject_B[1] > subject_A[1]:
+            subject_A = subject_B
+        subject_B = grab_data()
+        if subject_A[0] == subject_B[0]:
+            subject_B = grab_data()
+        
+        clear()
+        
+    else:
+        game_running = False
+       
+
+print(f'Game Over, the final score is {score}')
+
+
+
 
 
