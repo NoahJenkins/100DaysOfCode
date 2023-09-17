851f0e9383e4e385f80b6c08a1829a67a471c850
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-17 13:55:35 -0500

    packages

---
+++ Day 16 - OOP/2nd_Go/Packages_&_PyPi.py
@@ -0,0 +1,6 @@
+#PyPi stands for Python Package index. 
+#Look at documentation for instructions on install.
+# Here is the command to install PrettyTable:
+# python -m pip install -U prettytable
+
+import pretty
