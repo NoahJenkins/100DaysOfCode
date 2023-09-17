57474b895b6404fa29066a0e373b070b15da01b8
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-17 15:41:36 -0500

    Coffee_Machine OOP

--- Day 16 - OOP/2nd_Go/Coffee_Machine_OOP/Coffee_machine.py
+++ Day 16 - OOP/2nd_Go/Coffee_Machine_OOP/Coffee_machine.py
@@ -1,3 +1,21 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine
+menu = Menu()
+
+machine_running = True
+
+options = menu.get_items()
+user_choice = str(input(f'Which drink would you like to selct? {options}: ')).lower()
+if user_choice == 'off':
+    machine_running = False
+elif user_choice == 'report':
+    coffee_maker.report()
+    money_machine.report()
+else:
+    drink = menu.find_drink(user_choice)
+    if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+        coffee_maker(drink)
