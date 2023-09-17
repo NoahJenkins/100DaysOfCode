8cf35d8fe27a41735b4be138c66faed13fcc9dcf
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-17 14:14:59 -0500

    pretty tables.

--- Day 16 - OOP/2nd_Go/Packages_&_PyPi.py
+++ Day 16 - OOP/2nd_Go/Packages_&_PyPi.py
@@ -3,4 +3,16 @@
 # Here is the command to install PrettyTable:
 # python -m pip install -U prettytable
 
-import pretty
+from prettytable import PrettyTable
+table = PrettyTable()
+
+table.add_column('Pokemon Name', ['Pikachu', 'Squirtle', 'Charmander'])
+table.add_column('Pokemon Type', ['Eletric,', 'Water', 'Fire'])
+print(table)
+
+# the above (add column) changes are examples of methods. 
+
+#The code above is an example of a method, it is changing the text allignment from center to left
+table.align = 'l'
+print(table)
+#
