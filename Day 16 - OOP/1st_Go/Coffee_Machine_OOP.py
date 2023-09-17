7e4b10d4884907e4aaee8608ebf0bf868606f670
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-17 16:09:28 -0500

    day 16 complete

--- Day 16 - OOP/1st_Go/Coffee_Machine_OOP.py
+++ Day 16 - OOP/1st_Go/Coffee_Machine_OOP.py
@@ -25,3 +25,6 @@ while machine_running:
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
             coffee_maker.make_coffee(drink) 
 
+clear()
+
+
--- Day 16 - OOP/2nd_Go/Coffee_Machine_OOP/Coffee_machine.py
+++ Day 16 - OOP/2nd_Go/Coffee_Machine_OOP/Coffee_machine.py
@@ -1,21 +1,33 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+import os
+def clear():
+    """This function will cleae the terminal. Please note that you must 'import os' for this to work."""
+    # Clear the terminal screen for Windows
+    if os.name == 'nt':
+        os.system('cls')
+    # Clear the terminal screen for Unix/Linux/MacOS
+    else:
+        os.system('clear')
+
 
 coffee_maker = CoffeeMaker()
-money_machine = MoneyMachine
+money_machine = MoneyMachine()
 menu = Menu()
 
 machine_running = True
 
-options = menu.get_items()
-user_choice = str(input(f'Which drink would you like to selct? {options}: ')).lower()
-if user_choice == 'off':
-    machine_running = False
-elif user_choice == 'report':
-    coffee_maker.report()
-    money_machine.report()
-else:
-    drink = menu.find_drink(user_choice)
-    if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-        coffee_maker(drink)
+while machine_running:
+    options = menu.get_items()
+    user_choice = input(f"What drink would you like? ({options}): ")
+    if user_choice == "off":
+        machine_running = False
+    elif user_choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(user_choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink) 
+clear()
