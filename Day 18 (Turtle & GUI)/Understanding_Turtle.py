8142090c26fa2eb42ce97873b23dd6b2b0c860fd
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-19 07:49:45 -0500

    day 18 start

---
+++ Day 18 (Turtle & GUI)/Understanding_Turtle.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+from turtle import Screen
+
+timmy = Turtle()
+timmy.shape('turtle')
+timmy.color('red')
+
+
+#Challenge 1, make a square with the turtle.
+# def square_component():
+#     timmy.forward(100)
+#     timmy.right(90)
+# def make_square():
+#     square_component()
+#     square_component()
+#     square_component()  
+#     square_component()
+
+# make_square()
+
+for num in range(4):
+    timmy.forward(100)
+    timmy.right(90)
+
+# The exit screen on click has to happen last, so it is at the bottom of the file
+screen = Screen()
+screen.exitonclick()
