a40fc66700e20cd708afd922114a5160a1b4464f
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-19 15:18:31 -0500

    challenge 2, importing notes

---
+++ Day 18 (Turtle & GUI)/Importing_Modules.py
@@ -0,0 +1,18 @@
+# from turtle import Turtle
+# tim = Turtle()
+# terry = Turtle()
+# tom = Turtle()
+
+# This turtles the entire module
+# from turtle import *
+#Importing all is not best ppractice
+
+#How to Alias Modules
+import turtle as t
+tim =t.Turtle()
+
+# Not all modules are packaged with Python.
+# Turtule is packaged with Python. 
+
+# Other packaged are installed via PyPi
+# import heroes
--- Day 18 (Turtle & GUI)/Understanding_Turtle.py
+++ Day 18 (Turtle & GUI)/Understanding_Turtle.py
@@ -1,9 +1,9 @@
 from turtle import Turtle
 from turtle import Screen
 
-timmy = Turtle()
-timmy.shape('turtle')
-timmy.color('red')
+# timmy = Turtle()
+# timmy.shape('turtle')
+# timmy.color('red')
 
 
 #Challenge 1, make a square with the turtle.
@@ -18,10 +18,26 @@ timmy.color('red')
 
 # make_square()
 
-for num in range(4):
-    timmy.forward(100)
-    timmy.right(90)
+# for num in range(4):
+#     timmy.forward(100)
+#     timmy.right(90)
 
-# The exit screen on click has to happen last, so it is at the bottom of the file
-screen = Screen()
-screen.exitonclick()
+# # The exit screen on click has to happen last, so it is at the bottom of the file
+# screen = Screen()
+# screen.exitonclick()
+
+#Challenge 2:
+# timmy = Turtle()
+# dash = 15
+# skip = 5
+
+# for num in range(20):
+#     timmy.pendown()
+#     timmy.forward(dash)
+#     timmy.penup()
+#     timmy.forward(skip)
+
+# screen = Screen()
+# screen.exitonclick()
+
+# Challenge 3:
