d159a7d0632017d7578b1aa4268698d1ec356d42
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-20 06:38:42 -0500

    hurst painting start

---
+++ Day 18 (Turtle & GUI)/Hirst_Painting.py
@@ -0,0 +1,19 @@
+import colorgram
+
+# Extract 6 colors from an image.
+colors = colorgram.extract('painting.avif', 6)
+# RGB, HSL, and what proportion of the image was that color.
+first_color = colors[0]
+rgb = first_color.rgb # e.g. (255, 151, 210)
+
+# RGB and HSL are named tuples, so values can be accessed as properties.
+# These all work just as well:
+red = rgb[0]
+red = rgb.r
+
+from turtle import Turtle
+from turtle import Screen
+import random
+
+tim = Turtle()
+
