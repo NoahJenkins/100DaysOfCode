8394b9931a02b1ab8da43a364eb21bd57ed5f3a7
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-24 12:17:24 -0500

    player logic

--- Day 23 (Turtle Crossing Game)/player.py
+++ Day 23 (Turtle Crossing Game)/player.py
@@ -1,7 +1,31 @@
+from turtle import Turtle, Screen
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.listen()
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape('turtle')
+        self.color('black')
+        self.goto(STARTING_POSITION)
+        self.player_forward()
+
+    def player_forward(self):
+        self.forward(MOVE_DISTANCE)
+
+
+turtle = Player()
+
+screen.onkey(turtle.player_forward, 'Up')
+screen.exitonclick()
+
+
+
