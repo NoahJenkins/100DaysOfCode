f269c03e83d7991c278a6b0c7d6affd0e88db0b1
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-25 18:44:11 -0500

    with open notes

--- Day 24 (Files, Directories, and Paths)/Open_Read_Write_using_with_Keyword.py/main.py
+++ Day 24 (Files, Directories, and Paths)/Open_Read_Write_using_with_Keyword.py/main.py
@@ -9,8 +9,22 @@
 # file = open(path)
 
 path = "Day 24 (Files, Directories, and Paths)\\Open_Read_Write_using_with_Keyword.py\\my_file.txt"
-file = open(path)
+# file = open(path)
 
-contents = file.read()
-print(contents)
-file.close()
+# contents = file.read()
+# print(contents)
+# file.close()
+
+
+# Using the with keyword will close the file after it has been used. 
+with open(path, mode = "a") as file:
+    file.write('\nNew text')
+
+# modes:
+# w = write
+# r=read
+# a=append
+
+#This code will create a new file. 
+with open("new_file.txt", mode = "w") as file:
+    file.write('New text in new file. ')
