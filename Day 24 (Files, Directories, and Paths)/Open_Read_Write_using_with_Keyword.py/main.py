a6d440786fc5c3a6940d8d7fc3d5a79ec2a2a1d2
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-25 18:15:46 -0500

    working directory

--- Day 24 (Files, Directories, and Paths)/Open_Read_Write_using_with_Keyword.py/main.py
+++ Day 24 (Files, Directories, and Paths)/Open_Read_Write_using_with_Keyword.py/main.py
@@ -1,8 +1,11 @@
-import os
-cwd = os.getcwd()
-files = os.listdir(cwd)
-print(cwd, files)
+#The below is to show you the current directory level.
+# import os
+# cwd = os.getcwd()
+# files = os.listdir(cwd)
+# print(cwd, files)
 
 
+path = "C:\\Code\\100DaysOfCode\\Day 24 (Files, Directories, and Paths)\\Open_Read_Write_using_with_Keyword.py\\my_file.txt"
+file = open(path)
 
 # file = open("my_file.txt")
