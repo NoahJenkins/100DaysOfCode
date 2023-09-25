369d2d250c15af5427c419b95bae24ac7c680195
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-25 18:26:51 -0500

    directory error fix

--- Day 24 (Files, Directories, and Paths)/Open_Read_Write_using_with_Keyword.py/main.py
+++ Day 24 (Files, Directories, and Paths)/Open_Read_Write_using_with_Keyword.py/main.py
@@ -5,7 +5,12 @@
 # print(cwd, files)
 
 
-path = "C:\\Code\\100DaysOfCode\\Day 24 (Files, Directories, and Paths)\\Open_Read_Write_using_with_Keyword.py\\my_file.txt"
+# path = "C:\\Code\\100DaysOfCode\\Day 24 (Files, Directories, and Paths)\\Open_Read_Write_using_with_Keyword.py\\my_file.txt"
+# file = open(path)
+
+path = "Day 24 (Files, Directories, and Paths)\\Open_Read_Write_using_with_Keyword.py\\my_file.txt"
 file = open(path)
 
-# file = open("my_file.txt")
+contents = file.read()
+print(contents)
+file.close()
