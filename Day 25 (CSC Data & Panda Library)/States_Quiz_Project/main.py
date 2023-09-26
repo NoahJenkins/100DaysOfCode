b256b4d233e4a853e0bd0acfc53ce5dcc4fec96a
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-26 18:37:39 -0500

    guiz game working

--- Day 25 (CSC Data & Panda Library)/States_Quiz_Project/main.py
+++ Day 25 (CSC Data & Panda Library)/States_Quiz_Project/main.py
@@ -1,19 +1,53 @@
 import turtle
 import pandas
-import random
+from show_state import show_answer
 
+#write the correct guesses onto the map
+
+
+# Getting Data
+data = pandas.read_csv('50_states.csv')
+state_dict = data.to_dict()
+print(state_dict)
+state_list = data['state'].to_list()
+print(state_list)
+
+# Setting Up Screen
 screen = turtle.Screen()
 screen.title('U.S States Game')
 
-image = "blank_states_img.gif'"
+image = "blank_states_img.gif"
 screen.addshape(image)
 
 turtle.shape(image)
 
+scribe = show_answer()
+
+#Setting up Score
+score = 0
+guesses = []
+
+# Program Running
+game_running = True
+while game_running:
+    
+    answer_state = screen.textinput(title = "Guess the State", prompt= "What's another state's name?").title()
+    print(answer_state)
+
+    for state in state_list:
+        if answer_state == state:
+            score += 1
+            state_data = data[data['state'] == answer_state]
+            guesses.append(answer_state)
+            print(guesses)
+            scribe.write_answer(x=int(state_data['x']),y = int(state_data['y']), state = answer_state)
+        elif answer_state == 'Exit':
+            game_running
+        
+        if score == 50:
+            game_running = False
+        
 
 
-
-
-
-screen.exitonclick()
+screen.mainloop()
 
