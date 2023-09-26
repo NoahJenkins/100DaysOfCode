8b2f02eff18be0f56a9261bd23c746bc9541d98e
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-26 17:47:07 -0500

    us game start

---
+++ Day 25 (CSC Data & Panda Library)/States_Quiz_Project/main.py
@@ -0,0 +1,19 @@
+import turtle
+import pandas
+import random
+
+screen = turtle.Screen()
+screen.title('U.S States Game')
+
+image = "blank_states_img.gif'"
+screen.addshape(image)
+
+turtle.shape(image)
+
+
+
+
+
+
+screen.exitonclick()
+
