b75572b5f7f4450cfc32ee0b549eaf3d188bc5d3
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-25 19:05:06 -0500

    high score logic.

--- Day 24 (Files, Directories, and Paths)/Snake Project/scoreboard.py
+++ Day 24 (Files, Directories, and Paths)/Snake Project/scoreboard.py
@@ -8,7 +8,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('Day 24 (Files, Directories, and Paths)\\Snake Project\\data.txt') as data:
+            score_variable = data.read()
+            self.high_score = int(score_variable)
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -22,6 +24,10 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open('Day 24 (Files, Directories, and Paths)\\Snake Project\\data.txt', mode ="w") as data:
+                data.write(f'{self.high_score}')
+
+
         self.score = 0
         self.update_scoreboard()
 
---
+++ Day 24 (Files, Directories, and Paths)/Snake Project/test.py
@@ -0,0 +1,3 @@
+file = open('Day 24 (Files, Directories, and Paths)\\Snake Project\\data.txt')
+contents = file.read()
+print(contents)
