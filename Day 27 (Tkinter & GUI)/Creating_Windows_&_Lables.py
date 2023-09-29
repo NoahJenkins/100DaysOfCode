18452ded2384a57147b8953d171327a487cf98b7
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-28 19:14:38 -0500

    day 27 notes

--- Day 27 (Tkinter & GUI)/Creating_Windows_&_Lables.py
+++ Day 27 (Tkinter & GUI)/Creating_Windows_&_Lables.py
@@ -1,22 +1,32 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title('My 1st GUI Program')
 window.minsize(width=500,height=300)
 
 #Label
-label = tkinter.Label(text="I am a label :)", font=("Arial", 24, "bold"))
-label.pack(side="left")
+label =Label(text="I am a label :)", font=("Arial", 24, "bold"))
+
+def label_reveal():
+    label.pack(side="left")
 
 #Advange Arguments in Python:
 #You can create arguments with default values. 
-import turtle
+# import turtle
 
-tim = turtle.Turtle()
-tim.write('hello')
+# tim = turtle.Turtle()
+# tim.write('hello')
 
+#Button
+def button_click():
+    print('I got clicked')
 
+button =Button(text="click me", command = label_reveal)
+button.pack()
 
+input = Entry(width=10)
+input.pack()
+input.get()
 
 window.mainloop()
     
--- Day 27 (Tkinter & GUI)/args.py
+++ Day 27 (Tkinter & GUI)/args.py
@@ -1,7 +1,31 @@
-#Unlimited arugments
+#Unlimited (positional) arugments
 
 def add(*args):
+    sum = 0
     for n in args:
-        print(n)
+        sum += n
+    print(sum)
 
 add(1,2,3,4,5,6,7,8,9,10)
+
+#Many Keyword Arguments
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    print(kwargs["add"])
+    n=+ kwargs['add']
+    n*= kwargs['multiply']
+    print(n)
+
+calculate(2, add=3, multiply = 5)
+#output in a dictionary, which represents each keyword argument and it's value. 
+
+class Car:
+    def __init__(self, **kw) -> None:
+        self.make=kw.get["make"]
+        self.model = kw.get['model']
+        #benefit of get, is that if it is note called, it will return none as the value of the keyword. 
+
+my_car = Car(make="Nissan", model="GT-R")
+
+print(my_car.model, my_car.model)
