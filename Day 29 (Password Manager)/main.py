1d685e805abcb1a58922610574f724b4aeccf8c1
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-09-30 19:25:40 -0500

    Day 27 start

---
+++ Day 29 (Password Manager)/main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("MyPass Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+logo_image = PhotoImage(file='logo.png')
+canvas.create_image(100,100,image=logo_image)
+canvas.place()
+
+window.mainloop()
+
