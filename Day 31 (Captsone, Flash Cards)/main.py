7082674aef28d1445e5b036741309956e4702bfc
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-10-02 18:16:50 -0500

    enlish and flip over logic

--- Day 31 (Captsone, Flash Cards)/main.py
+++ Day 31 (Captsone, Flash Cards)/main.py
@@ -4,6 +4,7 @@ import random
 
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
 
 ######################  Create Flash Card #####################
 
@@ -15,16 +16,23 @@ to_learn = data.to_dict(orient="records")
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card= random.choice(to_learn)
     current_card["French"]
-    canvas.itemconfig(card_title, text= ("French"))
-    canvas.itemconfig(card_text, text = current_card["French"])
+    canvas.itemconfig(card_title, text= ("French"), fill = "black")
+    canvas.itemconfig(card_text, text = current_card["French"], fill = "black")
+    canvas.itemconfig(card_background,image = card_front_img )
+    flip_timer = window.after(3000, func=flip_card)
 
 
 ######################  Flip  Flash Card #####################
 
 def flip_card():
-    pass
+    global current_card
+    canvas.itemconfig(card_title,text="English", fill = "white")
+    canvas.itemconfig(card_text,text=current_card["English"], fill = "white")
+    canvas.itemconfig(card_background, image = card_back_image)
 
 
 
@@ -34,12 +42,13 @@ window = Tk()
 window.title("Flash Card Game")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-window.after(3000, func=flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
 #Canvas/Card
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image = card_front_img)
+card_back_image = PhotoImage(file = "images/card_back.png")
+card_background = canvas.create_image(400, 263, image = card_front_img)
 card_title = canvas.create_text(400, 150,text="", font = ("Arial", 40, "italic"))
 card_text = canvas.create_text(400, 263, text = "", font = ("Arial", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
