c7ea7d207d327ea06887819348bb223dbe37d10e
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-10-02 17:29:29 -0500

    Day 31 UI set up

--- Day 31 (Captsone, Flash Cards)/main.py
+++ Day 31 (Captsone, Flash Cards)/main.py
@@ -14,20 +14,20 @@ window.title("Flash Card Game")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 #Canvas/Card
-canvas = Canvas(width=800, height=800, bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image = card_front_img)
 canvas.create_text(400, 150,text="Title", font = ("Arial", 40, "italic"))
 canvas.create_text(400, 263, text = "word", font = ("Arial", 60, "bold"))
-canvas.grid(row=0, column=0)
+canvas.grid(row=0, column=0, columnspan=2)
 
 #Button Set Up. 
-wrong_logo = PhotoImage('images/wrong.png')
-wrong_but = Button(image=wrong_logo)
+wrong_logo = PhotoImage(file = 'images/wrong.png')
+wrong_but = Button(image=wrong_logo, highlightthickness=0)
 wrong_but.grid(row = 1, column=0)
 
-right_logo = PhotoImage('images/right.png')
-right_but = Button(image=right_logo)
+right_logo = PhotoImage(file = 'images/right.png')
+right_but = Button(image=right_logo, highlightthickness=0)
 right_but.grid(row = 1, column=1)
 
 
