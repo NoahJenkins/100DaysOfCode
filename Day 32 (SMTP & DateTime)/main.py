2c68243e4f9902c0a3df145367118e56bb0fed54
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-10-03 17:41:24 -0500

    day 32 smtp

--- Day 32 (SMTP & DateTime)/main.py
+++ Day 32 (SMTP & DateTime)/main.py
@@ -1,11 +1,12 @@
 import smtplib
 
 my_email = "dev892259@gmail.com"
-password = "abc123"
+from password import password
 
 connection = smtplib.SMTP('smtp.gmail.com')
 
 #This encrypts our email
 connection.starttls()
 connection.login(user = my_email, password= password)
-con
+connection.sendmail(from_addr=my_email, to_addrs="dev.two@yahoo.com", mesg = "Hello World")
+connection.close()
