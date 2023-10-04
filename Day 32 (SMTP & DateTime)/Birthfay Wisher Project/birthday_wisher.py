0a501f18b80f86cfdf4f34b47a7be9ca75c04ffa
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-10-03 19:08:06 -0500

    letter update

--- Day 32 (SMTP & DateTime)/Birthfay Wisher Project/birthday_wisher.py
+++ Day 32 (SMTP & DateTime)/Birthfay Wisher Project/birthday_wisher.py
@@ -14,16 +14,16 @@ birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, dat
 
 if today in birthdays_dict:
     birthday_person = birthdays_dict[today]
-    file_path = f"letter_templates/letter_{random.randint(1-3)}.txt"
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
     with open(file_path) as letter_file:
         contents = letter_file.read()
-        contents.replace("[NAME]", birthday_person['name'])
+        contents = contents.replace("[NAME]", birthday_person['name'])
 
     with smtplib.SMTP('smtp.gmail.com',587) as connection:
         connection.starttls()
         connection.login(user = my_email, password= password)
         connection.sendmail(from_addr=my_email,
-                            to_addrs=birthday_person['email'] 
+                            to_addrs=birthday_person['email'],
                             msg = f"Subject:Happy Birthday!\n\n{contents}")
 
 
