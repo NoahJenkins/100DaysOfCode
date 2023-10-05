c74526ff6c5c3344c6dd07dc7a56e541a5f9bd30
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-10-05 07:45:19 -0500

    day 34 Ui set up

--- Day 34 (API Practice)/ui.py
+++ Day 34 (API Practice)/ui.py
@@ -8,27 +8,25 @@ class QuizInterface:
         #Window
         self.window = Tk()
         self.window.title('Quizler')
-        self.window.config(bg=THEME_COLOR, padx=20)
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
 
         #Canvas/Question
         self.canvas = Canvas(width=300, height=250, bg="white")
-        self.canvas.create_text(150, 125, text=f'dummy text', font=("Arial", 20, "italic"))
-        self.canvas.grid(row=1, column=0, columnspan=2)
+        self.question_text = self.canvas.create_text(150, 125, text=f'dummy text', font=("Arial", 20, "italic"), fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
         
         #Score Label
-        self.score = Label(text=f'score: {0}', bg=THEME_COLOR, foreground="white", pady=20)
+        self.score = Label(text=f'score: {0}', bg=THEME_COLOR, foreground="white")
         self.score.grid(row=0, column=1)
     
-
         #Buttons
-        true_image = PhotoImage("images/true.image")
-        self.true_button = Button(image=true_image)
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, bg=THEME_COLOR, highlightthickness=0)
         self.true_button.grid(row=2, column=0)
 
-        false_image = PhotoImage("images/false.image")
-        self.false_button = Button(image=false_image)
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, bg=THEME_COLOR, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
-
 
 
         self.window.mainloop()
