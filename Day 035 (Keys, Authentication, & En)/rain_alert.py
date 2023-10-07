8d35c493e1fcfa1f0341db7b32573927e755c946
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-10-07 12:13:48 -0500

    day 35 progress

--- Day 035 (Keys, Authentication, & En)/rain_alert.py
+++ Day 035 (Keys, Authentication, & En)/rain_alert.py
@@ -2,3 +2,12 @@ import requests
 from api_key import KEY
 
 # // https://api.openweathermap.org/data/2.5/weather?q=Dallas,us&APPID=KEY
+
+parameters = { "q":"Dallas,us", 
+              "APPID":KEY
+             }
+
+response = requests.get("https://api.openweathermap.org/data/2.5/weather?", params=parameters)
+data = response.json()
+
+print(data)
