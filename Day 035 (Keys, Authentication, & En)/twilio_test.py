de0d7d45044bbdb05f62d8a911cbcd3e724218c3
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-10-07 12:26:29 -0500

    tw update

--- Day 035 (Keys, Authentication, & En)/twilio_test.py
+++ Day 035 (Keys, Authentication, & En)/twilio_test.py
@@ -2,11 +2,12 @@ from twilio.rest import Client
 from api_key import TW_SID, TW_AUTH
 
 account_sid = TW_SID
-auth_token = '[]'
+auth_token = TW_AUTH
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
   from_='+18333241411',
+  body= "Hello World",
   to='+18178980345'
 )
 
