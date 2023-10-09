3053db541439f5935376df7ea0619fb9f0cc7f0c
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-10-09 18:08:25 -0500

    SMS alert

--- Day 036 (Stock Alert Project)/stock_tracker.py
+++ Day 036 (Stock Alert Project)/stock_tracker.py
@@ -2,9 +2,10 @@
 import requests
 from api_key import alpha_api, news_api, TW_SID, TW_AUTH
 from twilio.rest import Client
+from em
 
 ##################### Global Variables #####################
-STOCK_NAME = "TSLA"
+STOCK_NAME = "LMT"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
@@ -27,39 +28,56 @@ stock_data = stock_response.json()
 time_series = stock_data["Time Series (Daily)"]
 latest_date = max(time_series.keys())
 close_price = float(time_series[latest_date]['4. close'])
-print(close_price)
+# print(close_price)
 
 #Yesterday Close
 dates = list(time_series.keys())
 dates.sort(reverse=True)
 yesterday = dates[1]
 yesterday_close = float(time_series[yesterday]['4. close'])
-print(yesterday_close)
+# print(yesterday_close)
 
 #Compare Values
 change_value = round(abs((close_price)-(yesterday_close)),2)
-print(change_value)
 change_pecent = round((change_value / yesterday_close),4) * 100
-print(change_pecent)
+# print(change_pecent)
+
+if close_price > yesterday_close:
+    change_direction = "+"
+elif close_price < yesterday_close:
+    change_direction = "-"
+else:
+    change_direction = ""
 
 ######################### News Data ##########################
 
-# news_params = {
-#     "apiKey": news_api,
-#     "q": STOCK_NAME
-# }
+news_params = {
+    "apiKey": news_api,
+    "q": STOCK_NAME
+}
 
-# news_response = requests.get(NEWS_ENDPOINT,news_params)
-# news_response.raise_for_status()
-# news_data = news_response.json()
+news_response = requests.get(NEWS_ENDPOINT,news_params)
+news_response.raise_for_status()
+news_data = news_response.json()
 # print(news_data)
+articles = news_data["articles"][:2]
+# print (articles)
 
 
+result_string = ""
+
+for i, article in enumerate(articles, start=1):
+    result_string += f"Headline : {article['title']}\n"
+    result_string += f"Brief : {article['description']}\n"
+    result_string += f"Content : {article['content']}\n"
+    result_string += f"URL : {article['url']}\n\n"
+
+# print(result_string)
 
 ######################### SMS Logic ##########################
-# account_sid = TW_SID
-# auth_token = TW_AUTH
-# client = Client(account_sid, auth_token)
+account_sid = TW_SID
+auth_token = TW_AUTH
+client = Client(account_sid, auth_token)
 
 # message = client.messages.create(
 #   from_='+18333241411',
@@ -69,3 +87,17 @@ print(change_pecent)
 
 # print(message.sid)
 
+######################### Conditional SMS ##########################
+
+# if change_pecent >= 5.0:
+#     message = client.messages.create(
+#     from_='+18333241411',
+#     body= f"{STOCK_NAME}:{change_direction}{change_pecent}\n\n{result_string}",
+#     to='+18178980345'
+# )
+
+# print(message.sid)
+
+# #Test code:
+body= f"{STOCK_NAME}:{change_direction}{change_pecent}\n\n{result_string}"
+print(body)
