ecc61291d33024b30eb9f071b6b4a5fb7de644d9
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-10-10 07:47:06 -0500

    Day 37 create graph

--- Day 037 (Advance Authentication)/HTTP_Request.py
+++ Day 037 (Advance Authentication)/HTTP_Request.py
@@ -7,14 +7,32 @@ from api_key import token, username
 
 
 #Creating Pixe.la User
-# pixela_endpoint = "https://pixe.la/v1/users"
+pixela_endpoint = "https://pixe.la/v1/users"
 
 # user_params = {
 #     "token": token,
 #     "username": username,
 #     "agreeTermsOfService": "yes",
 #     "notMinor": "yes",
-}
+# }
 
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+
+#Creating Graph
+graph_endpoint = f'{pixela_endpoint}/{username}/graphs'
+
+graph_config = {
+    "id": "graph1",
+    "name": "Python Graph",
+    "unit": "Commits",
+    "type": "int",
+    "color": "shibafu"
+}
+
+header = {
+    "X-USER-TOKEN": token
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=header)
+print(response.text)
