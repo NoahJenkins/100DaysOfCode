436089cc62919ee11f4fb820e33088b0c2ea1bcb
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-10-18 07:35:40 -0500

    Day 45 halfway point

--- Day 045 (Web Scraping)/bs4-start/main.py
+++ Day 045 (Web Scraping)/bs4-start/main.py
@@ -1,7 +1,42 @@
 from bs4 import BeautifulSoup
 
-with open("website.html") as fp:
-    soup = BeautifulSoup(fp, 'html.parser')
+with open("website.html", encoding='utf8') as file:
+    cotnents = file.read()
 
-soup = BeautifulSoup("<html>a web page</html>", 'html.parser')
-print(soup)
+soup = BeautifulSoup(cotnents, "html.parser")
+# print(soup.title.string)
+##################################################################
+
+# all_anchor_tags = soup.find_all(name="a")
+
+# for tag in all_anchor_tags:
+#     print(tag.getText())
+
+##################################################################
+
+# all_anchor_tags = soup.find_all(name="a")
+
+# for tag in all_anchor_tags:
+#     print(tag.get("href"))
+
+##################################################################
+
+# heading = soup.find(name="h1", id = "name")
+# print(heading)
+
+# section_heading = soup.find(name="h3",class_ = "heading")
+# print(section_heading)
+
+##################################################################
+
+# #CSS Selectors
+# company_url = soup.select_one(selector="p a")
+# print(company_url)
+
+# #CSS ID Selector
+# name = soup.select_one(selector="#name")
+# print(name)
+ 
+# #CSS Class Selector
+# headings = soup.select(".heading")
+# print(headings)
