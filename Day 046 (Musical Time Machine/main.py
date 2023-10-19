67407d46d37f7145f996224db17dcc470fade93b
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-10-19 14:44:06 -0500

    Day 46 start

--- Day 046 (Musical Time Machine/main.py
+++ Day 046 (Musical Time Machine/main.py
@@ -19,23 +19,57 @@ print(song_names)
 ######################################################
 #Spotify API access and authentication
 import spotipy
-from api_key import spotify_id, spotify_secret
+from spotipy.oauth2 import SpotifyOAuth
+from api_key import spotify_id, spotify_secret, sp_username
 
-#Spotify API access and authentication
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from api_key import spotify_id, spotify_secret, username
-
 
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
         redirect_uri="http://example.com",
-        client_id=spotify_id
-        client_secret= spotify_secret
+        client_id=spotify_id,
+        client_secret=spotify_secret,
         show_dialog=True,
         cache_path="token.txt",
-        username=username
+        username=sp_username, 
     )
 )
 user_id = sp.current_user()["id"]
+
+######################################################
+user_id = sp.current_user()["id"]
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+song_names = ["The list of song", "titles from your", "web scrape"]
+
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+######################################################
+        sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=spotify_id,
+        client_secret= spotify_secret,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+song_uris = ["The list of", "song URIs", "you got by", "searching Spotify"]
+
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+# print(playlist)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
