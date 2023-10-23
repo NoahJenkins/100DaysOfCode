827835901662c6054d2947bd661ad26282ee5218
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-10-23 17:57:53 -0500

    Day 48 progress

---
+++ Day 048 (Selenium WebDriver)/main.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+# Deprecated - no longer needed
+chrome_driver_path = "/Users/philippmuellauer/Development/chromedriver"
+
+# keeps chrome open
+chrome_options = webdriver.ChromeOptions()
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+chrome_options.add_argument(f"user-agent={user_agent}")
+chrome_options.add_experimental_option("detach", True)
+
+# driver = webdriver.Chrome(executable_path=chrome_driver_path)
+# driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=chrome_options)
+
+def test_eight_components():
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+    title = driver.title
+    assert title == "Web form"
+    driver.implicitly_wait(0.5)
+    text_box = driver.find_element(by=By.NAME, value="my-text")
+    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+    text_box.send_keys("Selenium")
+    submit_button.click()
+    message = driver.find_element(by=By.ID, value="message")
+    value = message.text
+    assert value == "Received!"
+
+    # Closes Chrome
+    # driver.quit()
+    driver.close()
+
+
+test_eight_components()
