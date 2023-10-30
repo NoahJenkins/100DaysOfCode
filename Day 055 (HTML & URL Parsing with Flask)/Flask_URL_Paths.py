00fefc62be9391c37fe2d2c531cbec3a24dc6a97
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-10-30 18:24:46 -0500

    Day 55 complete

--- Day 055 (HTML & URL Parsing with Flask)/Flask_URL_Paths.py
+++ Day 055 (HTML & URL Parsing with Flask)/Flask_URL_Paths.py
@@ -14,8 +14,28 @@ def hello_world():
     """
 
 ###############################################################################################################################
+#Editing HTLM with decorators
+
+def make_bold(function):
+    def wrapper_function():
+        return "<b>" + function() + "</b>"
+    return wrapper_function
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
+
 
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
 def bye():
     return "Bye!"
 
@@ -32,7 +52,30 @@ def greet(name):
     return f"Hello {name}!"
 
 ###############################################################################################################################
+#Advance Python Decorators Functions
 
+# class User:
+#     def __init__(self, name):
+#         self.name = name
+#         self.is_logged_in = False
+
+# def is_authenticated_decorator(function):
+#     def wrapper(*args, **kwargs):
+#         if args[0].is_logged_in == True:
+#             function(args[0])
+#     return wrapper
+
+# @is_authenticated_decorator
+# def create_blog_post(user):
+#     print(f"This is {user.name}'s new blog post.")
+
+# new_user = User("noah")
+# new_user.is_logged_in = True
+# create_blog_post(new_user)
+
+
+
+###############################################################################################################################
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ Day 055 (HTML & URL Parsing with Flask)/challenge.py
@@ -0,0 +1,14 @@
+inputs = eval(input()) # eval() creates list for inputs with format: [1,2,3]
+
+def logging_decorator(fn):
+  def wrapper(*args):
+    print(f"You called {fn.__name__}{args}")
+    result = fn(args[0], args[1], args[2])
+    print(f"It returned: {result}")
+  return wrapper
+
+@logging_decorator
+def a_function(a, b, c):
+  return a * b * c
+
+a_function(inputs[0], inputs[1], inputs[2])
