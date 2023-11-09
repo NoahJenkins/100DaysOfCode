ede69417b2a4303132d04eb81a4e0dd2a09a082f
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-11-08 18:35:07 -0600

    Day 63 complete

--- Day 063 (SQLite)/SQLite/main.py
+++ Day 063 (SQLite)/SQLite/main.py
@@ -1,8 +1,35 @@
-import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-db = sqlite3.connect("books-collection.db")
+app = Flask(__name__)
 
-cursor = db.cursor()
+##CREATE DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+
+# Create the extension
+db = SQLAlchemy()
+# Initialise the app with the extension
+db.init_app(app)
 
 
-cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+##CREATE TABLE
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
+
+# CREATE RECORD
+with app.app_context():
+    new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
+    db.session.add(new_book)
+    db.session.commit()
