418b6af949ad368a804bf62de25f8b69cc6e65ee
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-11-30 17:10:49 -0600

    Day 82

---
+++ Day 082 (Morse Code Converter)/morse_code.py
@@ -0,0 +1,26 @@
+# Morse code dictionary
+morse_code = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----',
+    ' ': '/'
+}
+
+def text_to_morse(text):
+    morse = ''
+    for char in text.upper():
+        if char in morse_code:
+            morse += morse_code[char] + ' '
+        else:
+            morse += char + ' '
+    return morse.strip()
+
+# Getting user input
+user_input = input("Enter the text to convert to Morse code: ")
+
+# Convert user input to Morse code
+morse_result = text_to_morse(user_input)
+
+print(f"Morse code: {morse_result}")
+
