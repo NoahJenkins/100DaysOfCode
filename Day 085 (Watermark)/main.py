0e20d6ee21416054cf2077255468441213aefd6e
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2023-12-04 16:50:55 -0600

    Day 85

--- Day 085 (Watermark)/main.py
+++ Day 085 (Watermark)/main.py
@@ -1,62 +1,57 @@
 from flask import Flask, render_template, request, redirect, url_for
-from werkzeug.utils import secure_filename
-from tkinter import Tk, filedialog
-from PIL import Image, ImageTk
-import io
-import base64
+from PIL import Image, ImageDraw, ImageFont
+import os
 
 app = Flask(__name__)
 
-app.config['UPLOAD_FOLDER'] = 'uploads/'
+# Function to add watermark to the image
+def add_watermark(input_image_path, output_image_path, watermark_text):
+    original_image = Image.open(input_image_path)
+    width, height = original_image.size
 
-def add_watermark(input_image, watermark_text):
-    # Open the image using PIL
-    image = Image.open(input_image)
+    # Creating a drawing context
+    drawing = ImageDraw.Draw(original_image)
 
-    # Add a text watermark using PIL
-    watermark = Image.new('RGBA', image.size, (255, 255, 255, 0))
-    watermark_text_draw = ImageDraw.Draw(watermark)
-    watermark_text_draw.text((10, 10), watermark_text, fill=(255, 255, 255, 128))
+    # Define font and position for watermark
+    font = ImageFont.load_default()
+    text_width, text_height = drawing.textsize(watermark_text, font)
+    text_position = ((width - text_width) // 2, (height - text_height) // 2)
 
-    # Combine the image with the watermark
-    watermarked_image = Image.alpha_composite(image.convert('RGBA'), watermark)
+    # Adding the watermark text to the image
+    drawing.text(text_position, watermark_text, fill="white", font=font)
 
-    return watermarked_image
+    # Save the image with watermark
+    original_image.save(output_image_path)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/upload', methods=['POST'])
-def upload():
+# Route to handle file upload and display the form
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect(request.url)
-        
+
         file = request.files['file']
+
         if file.filename == '':
             return redirect(request.url)
-        
+
         if file:
-            filename = secure_filename(file.filename)
-            file.save(app.config['UPLOAD_FOLDER'] + filename)
-            return redirect(url_for('process', filename=filename))
+            # Save the uploaded file
+            file_path = os.path.join('uploads', file.filename)
+            file.save(file_path)
 
-@app.route('/process/<filename>')
-def process(filename):
-    root = Tk()
-    root.withdraw()
-    filepath = app.config['UPLOAD_FOLDER'] + filename
+            # Watermark text
+            watermark_text = "Your Watermark Text"
 
-    watermark_text = filedialog.askstring("Watermark", "Enter watermark text:")
+            # Create a unique filename for the watermarked image
+            output_file_path = os.path.join('uploads', 'watermarked_' + file.filename)
 
-    watermarked_image = add_watermark(filepath, watermark_text)
+            # Add watermark to the uploaded image
+            add_watermark(file_path, output_file_path, watermark_text)
 
-    buffered = io.BytesIO()
-    watermarked_image.save(buffered, format='PNG')
-    img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+            return render_template('result.html', original_image=file.filename, watermarked_image='watermarked_' + file.filename)
 
-    return render_template('result.html', image=img_str)
+    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
